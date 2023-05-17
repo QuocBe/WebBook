03057882c0e3825a7d11121a2eb8d9ca645bf871
HaQuocPhap <haquocphap113@gmail.com>
2023-05-17 21:35:04 +0700

    finish

---
+++ app/Http/Middleware/CheckSigninAdmin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+
+class CheckSigninAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        //0:admin
+        if (Auth::check() && Auth::user()->level == 0) {
+            return $next($request);
+        } else {
+            return redirect()->route('signin');
+        }
+    }
+}
